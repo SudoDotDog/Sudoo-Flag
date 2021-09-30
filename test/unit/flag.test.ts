215ec5c6410a38f13f96534ceab36161ef3b6bba
WMXPY <wm@mengw.io>
2021-09-30 16:25:12 -0500

    add flag tests

--- test/unit/flag.test.ts
+++ test/unit/flag.test.ts
@@ -11,12 +11,25 @@ import { FlagManager } from "../../src";
 
 describe('Given {FlagManager} Class', (): void => {
 
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
     const chance: Chance.Chance = new Chance('flag-flag');
 
     it('should be able to construct', (): void => {
 
         const instance: FlagManager = FlagManager.empty();
         expect(instance).to.be.instanceOf(FlagManager);
+    });
+
+    it('should be able to construct with target list', (): void => {
+
+        const firstTarget: string = chance.string();
+        const secondTarget: string = chance.string();
+
+        const instance: FlagManager = FlagManager.fromTargets([
+            firstTarget,
+            secondTarget,
+        ]);
+
+        expect(instance).to.be.instanceOf(FlagManager);
+        expect(instance.targetLength).to.be.equal(2);
     });
 });
