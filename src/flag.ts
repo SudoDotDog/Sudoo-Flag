57aadf0fcbd0315885e22cd2da88e240fd04dd17
WMXPY <wm@mengw.io>
2021-09-30 14:50:23 -0500

    add falg functions

--- src/flag.ts
+++ src/flag.ts
@@ -9,9 +9,9 @@ import { FlagStorage } from "./declare";
 
 export class FlagManager {
 
-    public static empty(): FlagManager {
+    public static fromTargets(targets: string[]): FlagManager {
 
-        return new FlagManager([], []);
+        return new FlagManager(targets, []);
     }
 
     public static fromStorage(storage: FlagStorage): FlagManager {
@@ -26,5 +26,29 @@ export class FlagManager {
 
         this.targets = targets;
         this.flags = flags;
+    }
+
+    public get targetLength(): number {
+        return this.targets.length;
+    }
+    public get flagLength(): number {
+        return this.flags.length;
+    }
+
+    public getFlag(target: string): string[] {
+
+        const flagConfig: FlagConfig | undefined = this._getFlagConfig(target);
+
+        if (!flagConfig) {
+            return [];
+        }
+        return flagConfig.flags;
+    }
+
+    private _getFlagConfig(target: string): FlagConfig | undefined {
+
+        return this.flags.find((flag: FlagConfig) => {
+            return flag.target === target;
+        });
     }
 }
