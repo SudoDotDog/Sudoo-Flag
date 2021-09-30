098b1a3f5d469adbb5cfe76f0d895a05a27abbc4
WMXPY <wm@mengw.io>
2021-09-30 14:47:24 -0500

    add falg manger initial

--- src/flag.ts
+++ src/flag.ts
@@ -4,19 +4,27 @@
  * @description Flag
  */
 
+import { FlagConfig } from ".";
 import { FlagStorage } from "./declare";
 
 export class FlagManager {
 
     public static empty(): FlagManager {
-        return new FlagManager();
+
+        return new FlagManager([], []);
     }
 
     public static fromStorage(storage: FlagStorage): FlagManager {
-        return new FlagManager();
+
+        return new FlagManager(storage.targets, storage.flags);
     }
 
-    private constructor() {
-        // tslint:disable-next-line
+    private readonly targets: string[];
+    private readonly flags: FlagConfig[];
+
+    private constructor(targets: string[], flags: FlagConfig[]) {
+
+        this.targets = targets;
+        this.flags = flags;
     }
 }
